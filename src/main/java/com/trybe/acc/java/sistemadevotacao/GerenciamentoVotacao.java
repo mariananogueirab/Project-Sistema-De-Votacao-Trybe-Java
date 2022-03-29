3b5a98cea6218472d3ef71bebe3a6098a3af9d27
mariananbp <mariananbp@gmail.com>
2022-03-28 23:10:57 -0300

    class Gerenciamento Votacao

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,91 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.ArrayList;
+
 public class GerenciamentoVotacao {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  // um array com os objetos instanciados da classe PessoaCandidata
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  // um array com os objetos instanciados da classe PessoaEleitora
+  private ArrayList<String> cpfComputado = new ArrayList<String>();
+  // Esse array é responsável por manter os CPFs das pessoas eleitoras que já votaram
+  // cada pessoa eleitora só pode votar uma vez
+  private int totalVotos = 0;
+
+  /**
+   * Construtor.
+   */
+
+  public GerenciamentoVotacao() {};
+
+  // métodos
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    Boolean containNumber = false;
+    // referencia stackoverflow:
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numero) {
+        System.out.println("Número pessoa candidata já utilizado!");
+        containNumber = true;
+      }
+    }
+
+    if (containNumber == false) {
+      PessoaCandidata pessoa = new PessoaCandidata(nome, numero);
+      pessoasCandidatas.add(pessoa);
+    }
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    Boolean containCpf = false;
+    // referencia stackoverflow: percorrer arrai de objetos
+    for (PessoaEleitora pessoaEleitora : pessoasEleitoras) {
+      if (pessoaEleitora.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        containCpf = true;
+      }
+    }
+
+    if (containCpf == false) {
+      PessoaEleitora pessoa = new PessoaEleitora(nome, cpf);
+      pessoasEleitoras.add(pessoa);
+    }
+  }
+
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    Boolean containCpfEleitor = cpfComputado.contains(cpfPessoaEleitora);
+
+    if (containCpfEleitor == true) {
+      System.out.println("Pessoa eleitora já votou!");
+    } else {
+      for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+        if (pessoaCandidata.getNumero() == numeroPessoaCandidata) {
+          pessoaCandidata.receberVoto();
+        }
+      }
+      cpfComputado.add(cpfPessoaEleitora);
+      totalVotos += 1;
+    }
+  }
+
+  private Double calcularPorcentagemVotos(int indice) {
+    int votos = this.pessoasCandidatas.get(indice).getVotos();
+    Double votosEmPorcentagem = (votos * 100.0) / totalVotos;
+    return votosEmPorcentagem;
+  }
+
+  public void mostrarResultado() {
+    if (totalVotos <= 0) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    } else {
+      for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+        Double votosEmPorcentagem =
+            this.calcularPorcentagemVotos(this.pessoasCandidatas.indexOf(pessoaCandidata));
+        String nome = "Nome: " + pessoaCandidata.nome;
+        String votosPorc = "( " + votosEmPorcentagem + " )";
+        System.out.println(nome + " - " + pessoaCandidata.getVotos() + " votos " + votosPorc);
+      }
+      System.out.println("Total de votos: " + totalVotos);
+    }
+  }
 
 }

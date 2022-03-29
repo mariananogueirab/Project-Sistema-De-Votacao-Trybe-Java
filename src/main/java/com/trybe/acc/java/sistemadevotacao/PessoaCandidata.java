9004d121cabb50d9fc35f2ffabc2b7ba871891d8
mariananbp <mariananbp@gmail.com>
2022-03-28 23:10:02 -0300

    class pessoaCandidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,36 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  /**
+   * Constructor.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
 
 }
